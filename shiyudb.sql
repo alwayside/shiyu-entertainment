5857b192f14e18eabcc43b14d2dda0d3fd27f468
huangxin <huang.xin12@iwhalecloud.com>
2020-09-06 13:12:07 +0800

    database

--- shiyudb.sql
+++ shiyudb.sql
@@ -11,46 +11,97 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 30/07/2020 19:32:15
+ Date: 06/09/2020 12:33:07
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for shiyu_admin
+-- Table structure for admin
 -- ----------------------------
-DROP TABLE IF EXISTS `shiyu_admin`;
-CREATE TABLE `shiyu_admin`  (
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`, `account`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of shiyu_admin
+-- Table structure for album
 -- ----------------------------
-INSERT INTO `shiyu_admin` VALUES (2, 'admin', 'GeJkeev0FKnUGraJaQDJRg==', 'admin');
+DROP TABLE IF EXISTS `album`;
+CREATE TABLE `album`  (
+  `album_id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `album_cover` int(11) NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
+  `date_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`album_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for shiyu_article
+-- Table structure for album_photo
 -- ----------------------------
-DROP TABLE IF EXISTS `shiyu_article`;
-CREATE TABLE `shiyu_article`  (
+DROP TABLE IF EXISTS `album_photo`;
+CREATE TABLE `album_photo`  (
+  `album_id` int(11) NOT NULL,
+  `photo_id` int(11) NOT NULL,
+  PRIMARY KEY (`album_id`, `photo_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL,
   `desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `dateTime` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  `author_id` int(11) NULL DEFAULT NULL,
+  `author_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of shiyu_article
+-- Table structure for photo
 -- ----------------------------
-INSERT INTO `shiyu_article` VALUES (5, '新博客2', '<p>新博客2</p>', NULL, '2020-07-30 04:22:56');
-INSERT INTO `shiyu_article` VALUES (6, '新博客3', '<p>新博客3</p><p><br></p><p><br></p><p>update</p><p><br></p><p>444</p>', NULL, '2020-07-30 18:50:19');
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE `photo`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `picture_width` int(11) NULL DEFAULT NULL,
+  `picture_height` int(11) NULL DEFAULT NULL,
+  `image_type` int(11) NULL DEFAULT NULL,
+  `size` bigint(20) NULL DEFAULT NULL,
+  `file_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` bigint(20) NULL DEFAULT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `md5_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
+DROP TABLE IF EXISTS `video`;
+CREATE TABLE `video`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `width` int(11) NULL DEFAULT NULL,
+  `height` int(11) NULL DEFAULT NULL,
+  `video_type` int(11) NULL DEFAULT NULL,
+  `size` bigint(20) NULL DEFAULT NULL,
+  `file_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `create_time` bigint(20) NULL DEFAULT NULL,
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `md5_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `duration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
